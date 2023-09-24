cfb3c27217c864c8a1bf62af0a4429b0a6896a1a
Akshay <Akshay@DESKTOP-ONGUIDH.lan>
2023-09-24 16:55:19 -0400

    First Commit

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -16,6 +16,10 @@ public class JavaApplication {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println(" Attempt");
+        System.out.println(" Done Changes ");
+         System.out.println(" Second Changes ");
+        
+        
     }
     
 }
